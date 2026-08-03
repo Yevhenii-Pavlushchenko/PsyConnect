7db998cfc6d54875c9f9d2bd60a264cc202b3806
JustFun <paw93@ukr.net>
2026-08-03 13:34:32 +0300

    meta

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import "./globals.css";
 
-import { Inter } from "next/font/google";
+import { Manrope } from "next/font/google";
 import "modern-normalize/modern-normalize.css";
 
 import type { Metadata } from "next";
@@ -8,18 +8,20 @@ import type { Metadata } from "next";
 import TanstackProvider from "@/components/TanstackProvider/TanstackProvider";
 import Header from "@/components/Header/Header";
 
-const interSans = Inter({
-  variable: "--font-inter-sans",
+const manrope = Manrope({
+  variable: "--font-manrope",
   subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
-  title: "Travel Trucks",
-  description: "Application for truck rental",
+  title: "PsyConnect | Find Your Perfect Psychologist Online",
+  description: "Connect with verified professional therapists, psychologists, and life coaches online. Browse specialists, filter by approach, and book your session instantly to start your mental health journey.",
+  keywords: ["online therapy", "find psychologist", "mental health", "book therapist", "psychological help", "PsyConnect"],
   icons: {
-    icon: "/minibus-icon.svg",
+    icon: "/favicon-icon.jpg"
   },
 };
+
 
 export default function RootLayout({
   children,
@@ -27,7 +29,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className={`${interSans.variable}`}>
+    <html lang="en" className={`${manrope.variable}`}>
       <body>
         <TanstackProvider>
           <Header />
